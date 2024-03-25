b7e8091a187ab3c1631d3b07cfda498b70c97624
wanyos <juanjoromero9@gmail.com>
2024-03-25 19:16:50 +0100

    add config slint, delete files and folders

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,26 +1,24 @@
-/* eslint-env node */
-require('@rushstack/eslint-patch/modern-module-resolution')
-
 module.exports = {
   root: true,
-  'extends': [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended',
-    '@vue/eslint-config-prettier/skip-formatting'
-  ],
-  overrides: [
-    {
-      files: [
-        '**/__tests__/*.{cy,spec}.{js,ts,jsx,tsx}',
-        'cypress/e2e/**/*.{cy,spec}.{js,ts,jsx,tsx}',
-        'cypress/support/**/*.{js,ts,jsx,tsx}'
-      ],
-      'extends': [
-        'plugin:cypress/recommended'
-      ]
-    }
+  env: {
+    node: true,
+    browser: true
+  },
+  extends: [
+    'plugin:vue/vue3-recommended',
+    '@vue/standard',
+    '@vue/prettier',
+    'plugin:cypress/recommended'
   ],
   parserOptions: {
     ecmaVersion: 'latest'
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'vue/multi-word-component-names': 'off',
+    'vue/require-explicit-emits': 'off',
+    'vue/v-on-event-hyphenation': 'off',
+    'vue/no-lone-template': 'off'
   }
 }
