db2b5f3bdee402a2d898391bb5590b66e00c55d5
wanyos <juanjoromero9@gmail.com>
2024-04-23 12:33:07 +0200

    change in page report

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,55 @@
-<script setup>
-import { RouterView } from 'vue-router'
-</script>
-
 <template>
-  <h1>hello app</h1>
-  <RouterView />
+  <section class="section-app">
+
+    <header>
+
+      <section>
+        <label for="">Brand</label>
+      </section>
+
+      <section>
+        <label for="">Messages</label>
+      </section>
+
+    </header>
+
+    <aside>
+      <nav>
+        <ul>
+          <li>
+            <RouterLink to="/">Report</RouterLink>
+            <RouterLink to="/calendar">Calendar</RouterLink>
+            <RouterLink>My Calendar</RouterLink>
+            <RouterLink>Settings</RouterLink>
+          </li>
+        </ul>
+      </nav>
+    </aside>
+
+    <main>
+      <label for="">main</label>
+      <RouterView></RouterView>
+    </main>
+
+    <footer>
+      <p>wanyos@copyright.com</p>
+    </footer>
+  </section>
 </template>
 
-<style scoped></style>
+<script setup>
+import { RouterView, RouterLink } from 'vue-router'
+</script>
+
+<style scoped>
+
+.section-app {
+  grid-template-columns: 150px auto;
+  grid-template-rows: 50px auto 50px;
+}
+
+header {
+
+}
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,20 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import ReportView from '@/views/ReportView.vue'
+import CalendarView from '@/views/CalendarView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-   
+    {
+      path: '/',
+      name: 'report',
+      component: ReportView
+    },
+    {
+      path: '/calendar',
+      name: 'calendar',
+      component: CalendarView
+    }
   ]
 })
 
