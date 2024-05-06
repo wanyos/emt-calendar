65e403c24e2ffffdf2b674466741c578a9bed4e5
wanyos <juanjoromero9@gmail.com>
2024-05-06 18:11:04 +0200

    changes in settings view

--- src/views/SettingsView.vue
+++ src/views/SettingsView.vue
@@ -1,13 +1,9 @@
 <template>
-    <section>
-       <h1>Settings</h1>
-    </section>
- </template>
- 
- <script setup>
- 
- </script>
- 
- <style lang="css" scoped>
- 
- </style>
+  <section>
+    <h1>Settings</h1>
+  </section>
+</template>
+
+<script setup></script>
+
+<style lang="css" scoped></style>
